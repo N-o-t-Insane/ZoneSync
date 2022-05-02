0a554f549cbaef301a68cc7dd054dc9297a68464
n-o-T-I-n-s-a-n-e <rarestp2@gmail.com>
2022-05-02 08:03:45 +0300

    added timezones manually

--- ZoneSync.js
+++ ZoneSync.js
@@ -1,8 +1,164 @@
 const keep_alive = require('./keep_alive.js')
 
+const EST = -4;
+
+const additional = [
+    {
+        id: 'est',
+        zoneName: 'est',
+        countryCode: 'est',
+        countryName: 'est',
+        gmtOffset: EST *60,
+        estOffset: 0
+    },
+    {
+        id: 'gmt',
+        zoneName: 'gmt',
+        countryCode: 'gmt',
+        countryName: 'gmt',
+        gmtOffset: 0 *60,
+        estOffset: EST
+    },
+    {
+        id: 'cst',
+        zoneName: 'cst',
+        countryCode: 'cst',
+        countryName: 'cst',
+        gmtOffset: -5 *60,
+        estOffset: EST
+    },
+    {
+        id: 'cdt',
+        zoneName: 'cdt',
+        countryCode: 'cdt',
+        countryName: 'cdt',
+        gmtOffset: -5 *60,
+        estOffset: EST
+    },
+    {
+        id: 'cest',
+        zoneName: 'cest',
+        countryCode: 'cest',
+        countryName: 'cest',
+        gmtOffset: 2 *60,
+        estOffset: EST
+    },
+    {
+        id: 'aest',
+        zoneName: 'aest',
+        countryCode: 'aest',
+        countryName: 'aest',
+        gmtOffset: 10 *60,
+        estOffset: EST
+    },
+    {
+        id: 'acst',
+        zoneName: 'acst',
+        countryCode: 'acst',
+        countryName: 'acst',
+        gmtOffset: 9.5 *60,
+        estOffset: EST
+    },
+    {
+        id: 'aft',
+        zoneName: 'aft',
+        countryCode: 'aft',
+        countryName: 'aft',
+        gmtOffset: 4.5 *60,
+        estOffset: EST
+    },
+    {
+        id: 'akst',
+        zoneName: 'aft',
+        countryCode: 'aft',
+        countryName: 'aft',
+        gmtOffset: 4.5 *60,
+        estOffset: EST
+    },
+    {
+        id: 'ast',
+        zoneName: 'aft',
+        countryCode: 'aft',
+        countryName: 'aft',
+        gmtOffset: -4 *60,
+        estOffset: EST
+    },
+    {
+        id: 'cat',
+        zoneName: 'cat',
+        countryCode: 'cat',
+        countryName: 'cat',
+        gmtOffset: 2 *60,
+        estOffset: EST
+    },
+    {
+        id: 'cet',
+        zoneName: 'cet',
+        countryCode: 'cet',
+        countryName: 'cet',
+        gmtOffset: 1 *60,
+        estOffset: EST
+    },
+    {
+        id: 'eat',
+        zoneName: 'eat',
+        countryCode: 'eat',
+        countryName: 'eat',
+        gmtOffset: 3 *60,
+        estOffset: EST
+    },
+    {
+        id: 'msk',
+        zoneName: 'msk',
+        countryCode: 'msk',
+        countryName: 'msk',
+        gmtOffset: 3 *60,
+        estOffset: EST
+    },
+    {
+        id: 'eet',
+        zoneName: 'eet',
+        countryCode: 'eet',
+        countryName: 'eet',
+        gmtOffset: 2 *60,
+        estOffset: EST
+    },
+    {
+        id: 'mst',
+        zoneName: 'mst',
+        countryCode: 'mst',
+        countryName: 'mst',
+        gmtOffset: -7 *60,
+        estOffset: EST
+    },
+    {
+        id: 'pst',
+        zoneName: 'pst',
+        countryCode: 'pst',
+        countryName: 'pst',
+        gmtOffset: -8 *60,
+        estOffset: EST
+    },
+    {
+        id: 'wat',
+        zoneName: 'wat',
+        countryCode: 'wat',
+        countryName: 'wat',
+        gmtOffset: 1 *60,
+        estOffset: EST
+    },
+    {
+        id: 'wet',
+        zoneName: 'wet',
+        countryCode: 'wet',
+        countryName: 'wet',
+        gmtOffset: 0 *60,
+        estOffset: EST
+    },
+]
+
 const config = require(`./config.json`);
 
-const EST = -4;
 
 let zones = [];
 
@@ -32,38 +188,7 @@ axios(zoneConfig)
         }
         zones.push(zoneObject)
     }
-    zones.push({
-        id: 'est',
-        zoneName: 'est',
-        countryCode: 'est',
-        countryName: 'est',
-        gmtOffset: EST *60,
-        estOffset: 0
-    })
-    zones.push({
-        id: 'gmt',
-        zoneName: 'gmt',
-        countryCode: 'gmt',
-        countryName: 'gmt',
-        gmtOffset: 0 *60,
-        estOffset: EST
-    })
-    zones.push({
-        id: 'cst',
-        zoneName: 'cst',
-        countryCode: 'cst',
-        countryName: 'cst',
-        gmtOffset: -5 *60,
-        estOffset: EST
-    })
-    zones.push({
-        id: 'cdt',
-        zoneName: 'cdt',
-        countryCode: 'cdt',
-        countryName: 'cdt',
-        gmtOffset: -4 *60,
-        estOffset: EST
-    })
+    zones = zones.concat(additional)
   //console.log(zones);
 })
 .catch(function (error) {
@@ -90,7 +215,7 @@ client.on("ready", () => {
 client.on("messageCreate", message => {
     const args = message.content.slice((`t!`).lenght).trim().split(/ +/);
     const command = args.shift().toLowerCase();
-    if (command === "t!convert") {
+    if (command === "t!convert" || command === "convert") {
         time_in = args[0].split(`:`);
         time_h = parseInt(time_in[0]);
         time_m = parseInt(time_in[1]);
---
+++ additional.js
@@ -0,0 +1,154 @@
+export const additional = [
+    {
+        id: 'est',
+        zoneName: 'est',
+        countryCode: 'est',
+        countryName: 'est',
+        gmtOffset: EST *60,
+        estOffset: 0
+    },
+    {
+        id: 'gmt',
+        zoneName: 'gmt',
+        countryCode: 'gmt',
+        countryName: 'gmt',
+        gmtOffset: 0 *60,
+        estOffset: EST
+    },
+    {
+        id: 'cst',
+        zoneName: 'cst',
+        countryCode: 'cst',
+        countryName: 'cst',
+        gmtOffset: -5 *60,
+        estOffset: EST
+    },
+    {
+        id: 'cdt',
+        zoneName: 'cdt',
+        countryCode: 'cdt',
+        countryName: 'cdt',
+        gmtOffset: -5 *60,
+        estOffset: EST
+    },
+    {
+        id: 'cest',
+        zoneName: 'cest',
+        countryCode: 'cest',
+        countryName: 'cest',
+        gmtOffset: 2 *60,
+        estOffset: EST
+    },
+    {
+        id: 'aest',
+        zoneName: 'aest',
+        countryCode: 'aest',
+        countryName: 'aest',
+        gmtOffset: 10 *60,
+        estOffset: EST
+    },
+    {
+        id: 'acst',
+        zoneName: 'acst',
+        countryCode: 'acst',
+        countryName: 'acst',
+        gmtOffset: 9.5 *60,
+        estOffset: EST
+    },
+    {
+        id: 'aft',
+        zoneName: 'aft',
+        countryCode: 'aft',
+        countryName: 'aft',
+        gmtOffset: 4.5 *60,
+        estOffset: EST
+    },
+    {
+        id: 'akst',
+        zoneName: 'aft',
+        countryCode: 'aft',
+        countryName: 'aft',
+        gmtOffset: 4.5 *60,
+        estOffset: EST
+    },
+    {
+        id: 'ast',
+        zoneName: 'aft',
+        countryCode: 'aft',
+        countryName: 'aft',
+        gmtOffset: -4 *60,
+        estOffset: EST
+    },
+    {
+        id: 'cat',
+        zoneName: 'cat',
+        countryCode: 'cat',
+        countryName: 'cat',
+        gmtOffset: 2 *60,
+        estOffset: EST
+    },
+    {
+        id: 'cet',
+        zoneName: 'cet',
+        countryCode: 'cet',
+        countryName: 'cet',
+        gmtOffset: 1 *60,
+        estOffset: EST
+    },
+    {
+        id: 'eat',
+        zoneName: 'eat',
+        countryCode: 'eat',
+        countryName: 'eat',
+        gmtOffset: 3 *60,
+        estOffset: EST
+    },
+    {
+        id: 'msk',
+        zoneName: 'msk',
+        countryCode: 'msk',
+        countryName: 'msk',
+        gmtOffset: 3 *60,
+        estOffset: EST
+    },
+    {
+        id: 'eet',
+        zoneName: 'eet',
+        countryCode: 'eet',
+        countryName: 'eet',
+        gmtOffset: 2 *60,
+        estOffset: EST
+    },
+    {
+        id: 'mst',
+        zoneName: 'mst',
+        countryCode: 'mst',
+        countryName: 'mst',
+        gmtOffset: -7 *60,
+        estOffset: EST
+    },
+    {
+        id: 'pst',
+        zoneName: 'pst',
+        countryCode: 'pst',
+        countryName: 'pst',
+        gmtOffset: -8 *60,
+        estOffset: EST
+    },
+    {
+        id: 'wat',
+        zoneName: 'wat',
+        countryCode: 'wat',
+        countryName: 'wat',
+        gmtOffset: 1 *60,
+        estOffset: EST
+    },
+    {
+        id: 'wet',
+        zoneName: 'wet',
+        countryCode: 'wet',
+        countryName: 'wet',
+        gmtOffset: 0 *60,
+        estOffset: EST
+    },
+  ]
